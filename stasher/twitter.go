42dbdcfc08b6629fcf99437b5c38d9d2615933bc
streeter <streeter@ebi.ac.uk>
2016-12-03 21:15:52 +0000

    Filled out API for getting tweets

--- stasher/twitter.go
+++ stasher/twitter.go
@@ -3,7 +3,11 @@ package stasher
 import (
   "context"
   "github.com/dghubble/go-twitter/twitter"
-  //"golang.org/x/oauth2"
+  "golang.org/x/oauth2"
+  "fmt"
+  "net/http"
+  "log"
+  "time"
 )
 
 type TwitterStasher interface {
@@ -16,6 +20,94 @@ type twitterGetter interface {
   user(int64) *twitter.User
   userTimeline(*twitter.UserTimelineParams) []twitter.Tweet
   oembed(*twitter.Tweet) *twitter.OEmbedTweet
+}
+
+func SyncTwitter(ctx context.Context, userId int64, stasher TwitterStasher, accessToken string) (err error) {
+  defer func() {
+    if r := recover(); r != nil {
+      var ok bool
+      err, ok = r.(error)
+      if !ok {
+        err = fmt.Errorf("pkg: %v", r)
+      }
+    }
+  }()
+
+  config := &oauth2.Config{}
+  token := &oauth2.Token{AccessToken: accessToken}
+  httpClient := config.Client(ctx, token)
+  twitterClient := (*twitterClient)(twitter.NewClient(httpClient))
+
+  syncTwitter(ctx, userId, stasher, twitterClient)
+  return nil
+}
+
+type twitterClient twitter.Client;
+
+func (client *twitterClient) user(userId int64) *twitter.User {
+  c := (*twitter.Client)(client)
+  var user *twitter.User
+  var err error
+  for try := 0; try < 2; try++ {
+    var res *http.Response
+    user, res, err = c.Users.Show(&twitter.UserShowParams{UserID: userId})
+    if res != nil && res.StatusCode == 420 || res.StatusCode == 429 {
+      log.Println("Rate limited by Twitter. Sleeping for 15 minutes...")
+      time.Sleep(15 * time.Minute)
+      continue
+    }
+    break
+  }
+  if err != nil {
+    panic(err)
+  }
+  return user
+}
+
+func (client *twitterClient) userTimeline(p *twitter.UserTimelineParams) []twitter.Tweet {
+  c := (*twitter.Client)(client)
+  var t []twitter.Tweet
+  var err error
+  for try := 0; try < 2; try++ {
+    var res *http.Response
+    t, res, err = c.Timelines.UserTimeline(&twitter.UserTimelineParams{})
+    if res != nil && res.StatusCode == 420 || res.StatusCode == 429 {
+      log.Println("Rate limited by Twitter. Sleeping for 15 minutes...")
+      time.Sleep(15 * time.Minute)
+      continue
+    }
+    break
+  }
+  if err != nil {
+    panic(err)
+  }
+  return t
+}
+
+func (client *twitterClient) oembed(t *twitter.Tweet) *twitter.OEmbedTweet {
+  c := (*twitter.Client)(client)
+  var o *twitter.OEmbedTweet
+  var err error
+  p := &twitter.StatusOEmbedParams{
+    ID: t.ID,
+    OmitScript: twitter.Bool(true),
+    HideThread: twitter.Bool(true),
+    HideMedia: twitter.Bool(false),
+  }
+  for try := 0; try < 2; try++ {
+    var res *http.Response
+    o, res, err = c.Statuses.OEmbed(p)
+    if res != nil && res.StatusCode == 420 || res.StatusCode == 429 {
+      log.Println("Rate limited by Twitter. Sleeping for 15 minutes...")
+      time.Sleep(15 * time.Minute)
+      continue
+    }
+    break
+  }
+  if err != nil {
+    panic(err)
+  }
+  return o
 }
 
 func syncTwitter(ctx context.Context, userId int64, stasher TwitterStasher, getter twitterGetter) {
